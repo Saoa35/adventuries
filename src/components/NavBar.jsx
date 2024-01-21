db812d8438ae9dfc7163be915801496f295ecc7f
Saoa35 <saoamah@gmail.com>
2024-01-21 18:32:06 +0100

    + created Button comp.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 function NavBar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenue = () => setClick(false);
+
   return (
     <>
       <nav className="navbar">
@@ -9,7 +14,43 @@ function NavBar() {
           <Link to="/" className="navbar-logo">
             TRVL <i className="fab fa-typo3" />
           </Link>
-          <div className="menu-icon"></div>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menue active" : "nav-menue"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenue}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/services"
+                className="nav-links"
+                onClick={closeMobileMenue}
+              >
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenue}
+              >
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/sign-up"
+                className="nav-links-mobile"
+                onClick={closeMobileMenue}
+              >
+                Sign Up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
