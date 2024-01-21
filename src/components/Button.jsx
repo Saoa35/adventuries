8f406464a00b9905ce374cfe10421e18374f0216
Saoa35 <saoamah@gmail.com>
2024-01-21 20:45:39 +0100

    + created HeroSection comp.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Button.css";
+import { Link } from "react-router-dom";
 
 const STYLES = ["btn--primary", "btn--outline"];
 
@@ -15,4 +16,18 @@ export const Button = ({
   const checkButtonStyle = STYLES.includes(buttonStyle)
     ? buttonStyle
     : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <Link to="sign-up" className="btn-mobile">
+      <button
+        className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+        onClick={onClick}
+        type={type}
+      >
+        {children}
+      </button>
+    </Link>
+  );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,44 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./NavBar.css";
 
 function NavBar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
-  const closeMobileMenue = () => setClick(false);
+
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
-            TRVL <i className="fab fa-typo3" />
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            TRVL
+            <i class="fab fa-typo3" />
           </Link>
           <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
-          <ul className={click ? "nav-menue active" : "nav-menue"}>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="/" className="nav-links" onClick={closeMobileMenue}>
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
               </Link>
             </li>
@@ -27,7 +46,7 @@ function NavBar() {
               <Link
                 to="/services"
                 className="nav-links"
-                onClick={closeMobileMenue}
+                onClick={closeMobileMenu}
               >
                 Services
               </Link>
@@ -36,21 +55,23 @@ function NavBar() {
               <Link
                 to="/products"
                 className="nav-links"
-                onClick={closeMobileMenue}
+                onClick={closeMobileMenu}
               >
                 Products
               </Link>
             </li>
-            <li className="nav-item">
+
+            <li>
               <Link
                 to="/sign-up"
                 className="nav-links-mobile"
-                onClick={closeMobileMenue}
+                onClick={closeMobileMenu}
               >
                 Sign Up
               </Link>
             </li>
           </ul>
+          {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </nav>
     </>
