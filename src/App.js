60b5dc03b36ae71d43c525b6e7be44eb947d27d9
Saoa35 <saoamah@gmail.com>
2024-01-23 17:10:42 +0100

    + added styles to HeroSection.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar";
+import Home from "./components/pages/Home";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <BrowserRouter>
         <NavBar />
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
